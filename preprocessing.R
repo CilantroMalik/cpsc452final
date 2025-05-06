05ed706b9b3dc9083225532dd5852c9a397b6b1a
Ro Malik <49294844+CilantroMalik@users.noreply.github.com>
2025-05-05 23:59:51 -0400

    Add files via upload

---
+++ preprocessing.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(Seurat)
+library(data.table)
+library(hdWGCNA)
+
+dat <- readRDS("/gpfs/gibbs/pi/gerstein/rm2686/MultiomeSexDifferenceAnalysis/RM-sexDiffAnalysis/allCountsSZBD.rds")
+meta <- readRDS("/gpfs/gibbs/pi/gerstein/rm2686/MultiomeSexDifferenceAnalysis/RM-sexDiffAnalysis/allMetaSZBD.rds")
+
+wgcna <- readRDS("/gpfs/gibbs/pi/gerstein/rm2686/MultiomeSexDifferenceAnalysis/RM-sexDiffAnalysis/sczWGCNA/auto/Oligo/p0-mainNetwork.rds")
+tom <- GetTOM(wgcna) %>% as.data.frame
+metacells <- GetMetacellObject(wgcna)
+
+# extract data and add metadata
+exprMat <- metacells@assays$RNA@layers$data %>% as.data.frame
+rownames(exprMat) <- rownames(metacells)
+colnames(exprMat) <- colnames(metacells)
+
+sampleMeta <- meta %>% distinct(sample, .keep_all=T)
+exprMeta <- metacells@meta.data
+exprMeta$sex <- sampleMeta$sex[match(exprMeta$sample, sampleMeta$sample)]
+exprMeta$disorder <- sampleMeta$disorder[match(exprMeta$sample, sampleMeta$sample)]
+exprMeta$age <- sampleMeta$age[match(exprMeta$sample, sampleMeta$sample)]
+exprMeta$PMI <- sampleMeta$PMI[match(exprMeta$sample, sampleMeta$sample)]
+
+genes <- wgcna@misc$Oligo$wgcna_degrees$gene_name
+exprMat <- exprMat[genes,]
+
+# output to files
+fwrite(exprMat, "Documents/cpsc452/final/oligo-SCZ-metacellExpr.csv", row.names = T)
+fwrite(exprMeta, "Documents/cpsc452/final/oligo-SCZ-meta.csv", row.names = T)
+fwrite(tom, "Documents/cpsc452/final/oligo-SCZ-tom.csv", row.names = T)
+
+test <- fread("Documents/cpsc452/final/oligo-SCZ-metacellExpr.csv", data.table = F)
+
+
+# plot TOM thresholds vs number of links preserved to try and gauge what could be a good value of the hyperparameter
+thresholds <- seq(from=0, to=1, by=0.005)
+tomFrac <- c()
+for (thr in thresholds) {
+  tomFrac <- c(tomFrac, sum(tom > thr) / (nrow(tom)*ncol(tom)))
+}
+thresholdingVals <- data.frame(threshold=thresholds, frac=tomFrac)
+ggplot(thresholdingVals, aes(x=threshold, y=frac)) + geom_line()
